9b3e4f72aea2ae16207d4b085e6faff68d85f2bf
brunofbpaula <deepcooper@outlook.com>
2024-04-26 14:09:01 -0300

    config: add required dependencies

--- src/test/java/org/estudos/br/ConsultaIBGETest.java
+++ src/test/java/org/estudos/br/ConsultaIBGETest.java
@@ -3,10 +3,13 @@ package org.estudos.br;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ConsultaIBGETest {
     private static final String ESTADOS_API_URL = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/";
@@ -29,5 +32,34 @@ public class ConsultaIBGETest {
         HttpURLConnection connection = (HttpURLConnection) new URL(ESTADOS_API_URL + uf).openConnection();
         int statusCode = connection.getResponseCode();
         assertEquals(200, statusCode, "O status code da resposta da API deve ser 200 (OK)");
+    }
+
+    @Test
+    @DisplayName("Teste para consultar distrito")
+    public void testConsultarDistrito() throws IOException {
+
+        // Número do distrito de Águas de Santa Bárbara
+        int distrito = 350055005;
+        String expectedReponse = "[" +
+                "{\"id\":350055005," +
+                "\"nome\":\"Águas de Santa Bárbara\"," +
+                "\"municipio\":{\"id\":3500550,\"nome\":\"Águas de Santa Bárbara\"," +
+                "\"microrregiao\":{\"id\":35022,\"nome\":\"Avaré\"," +
+                "\"mesorregiao\":{\"id\":3504,\"nome\":\"Bauru\"," +
+                "\"UF\":{\"id\":35,\"sigla\":\"SP\",\"nome\":\"São Paulo\",\"" +
+                "regiao\":{\"id\":3,\"sigla\":\"SE\",\"nome\":\"Sudeste\"}}}}," +
+                "\"regiao-imediata\":{\"id\":350007,\"nome\":\"Avaré\"," +
+                "\"regiao-intermediaria\":{\"id\":3502,\"nome\":\"Sorocaba\"," +
+                "\"UF\":{\"id\":35,\"sigla\":\"SP\",\"nome\":\"São Paulo\",\"" +
+                "regiao\":{\"id\":3,\"sigla\":\"SE\",\"nome\":\"Sudeste\"}}}}}}]";
+
+        HttpURLConnection connectionMock = mock(HttpURLConnection.class);
+        when(connectionMock.getInputStream()).thenReturn(new ByteArrayInputStream(expectedReponse.getBytes()));
+        when(connectionMock.getResponseCode()).thenReturn(HttpURLConnection.HTTP_OK);
+
+        String response = ConsultaIBGE.consultarDistrito(distrito);
+        assert !response.isEmpty();
+        assertEquals(expectedReponse, response);
+
     }
 }
